99e94b7379d8b0b91eee5e6ed363166ad11bbbaa
Auvigoo <auvigooa@gmail.com>
2023-04-05 15:06:00 -0400

    Fix delete Employee (by using the Lives relation) Co-authored-by: Shriman Vinayagamoorthy <Shriman30@users.noreply.github.com>

--- employees/delete.php
+++ employees/delete.php
@@ -1,7 +1,11 @@
 <?php require_once '../database.php'; 
 
-$statement = $conn->prepare("DELETE FROM hbc353_4.Employees WHERE Medicare_Number = :Medicare_Number;");
-$statement->bindParam(":Medicare_Number", $_GET["Medicare_Number"]);
-$statement->execute();
+$statement1 = $conn->prepare("DELETE FROM hbc353_4.Lives WHERE Medicare_Number = :Medicare_Number;");
+$statement1->bindParam(":Medicare_Number", $_GET["Medicare_Number"]);
+$statement1->execute();
+
+$statement2 = $conn->prepare("DELETE FROM hbc353_4.Employees WHERE Medicare_Number = :Medicare_Number;");
+$statement2->bindParam(":Medicare_Number", $_GET["Medicare_Number"]);
+$statement2->execute();
 header("Location: .");
 ?>
