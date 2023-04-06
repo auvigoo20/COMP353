8e33fc6af5fa4a081f8c3203102ea72aca8794a4
Auvigoo <auvigooa@gmail.com>
2023-04-06 14:14:18 -0400

    Remove redundant query to delete employees since ON DELETE CASCADE has been added in the db

--- employees/delete.php
+++ employees/delete.php
@@ -1,9 +1,4 @@
 <?php require_once '../database.php'; 
-
-$statement1 = $conn->prepare("DELETE FROM hbc353_4.Lives WHERE Medicare_Number = :Medicare_Number;");
-$statement1->bindParam(":Medicare_Number", $_GET["Medicare_Number"]);
-$statement1->execute();
-
 $statement2 = $conn->prepare("DELETE FROM hbc353_4.Employees WHERE Medicare_Number = :Medicare_Number;");
 $statement2->bindParam(":Medicare_Number", $_GET["Medicare_Number"]);
 $statement2->execute();
