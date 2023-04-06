cb3bf56ca3f8dd9e0d5fa832c4cbbfa6f90f1664
Auvigoo <auvigooa@gmail.com>
2023-04-06 14:37:03 -0400

    Implement delete facilities

--- facilities/delete.php
+++ facilities/delete.php
@@ -1,2 +1,6 @@
-<?php require_once '../database.php';
+<?php require_once '../database.php'; 
+$statement2 = $conn->prepare("DELETE FROM hbc353_4.Facilities WHERE Name = :Name;");
+$statement2->bindParam(":Name", $_GET["Name"]);
+$statement2->execute();
+header("Location: .");
 ?>
