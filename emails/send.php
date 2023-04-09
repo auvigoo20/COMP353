071c34f713e604797747ebe93b16f4ff8627c6b0
YuriTarded12 <57156001+YuriTarded12@users.noreply.github.com>
2023-04-08 21:26:00 -0400

    Update send.php

--- emails/send.php
+++ emails/send.php
@@ -11,8 +11,22 @@ $statement->bindParam(':Medicare_Number', $Medicare_Number);
 $statement->execute();
 $employee = $statement->fetch(PDO::FETCH_ASSOC);
 $Email = $employee["Email"];
-mail($Email,$Subject,$Body,"From:353Medical@hotmail.com");
-if(isset($_SERVER['HTTP_REFERER'])) {
-    $previous = $_SERVER['HTTP_REFERER'];
+if (mail($Email, $Subject, $Body, "From:353Medical@hotmail.com")) {
+    if(strlen($Body)>80){
+        $Body=substr($Body, 0, 80);
+    }
+    $statement = $conn->prepare("INSERT INTO hbc353_4.Email (Medicare_Number, Facility_Name, Date, Subject, Body) VALUES(:Medicare_Number, :Facility_Name, :Date, :Subject, :Body)");
+    $statement->bindParam(':Medicare_Number', $Medicare_Number);
+    $statement->bindParam(':Facility_Name', $Facility_Name);
+    $statement->bindParam(':Subject', $Subject);
+    $statement->bindParam(':Body', $Body);
+    $statement->bindParam(':Date', $Date);
+    $statement->execute();
+    $employee = $statement->fetch(PDO::FETCH_ASSOC);
+    if (isset($_SERVER['HTTP_REFERER'])) {
+        $previous = $_SERVER['HTTP_REFERER'];
+    }
 }
+
+
 ?>
