402ac6e6ddcfee07077172e8506d4a92a044baa1
Auvigoo <auvigooa@gmail.com>
2023-04-10 23:20:33 -0400

    Fix email compatability in production
    Co-authored-by: Shriman Vinayagamoorthy <Shriman30@users.noreply.github.com>

--- emails/send.php
+++ emails/send.php
@@ -4,13 +4,15 @@ function sendAndLog($Medicare_Number, $Email, $Facility_Name, $Subject, $Body)
     include('../database.php');
     
     $Date = date('Y-m-d');
-    if (mail($Email, $Subject, $Body, "from:comp353health@gmail.com")) {
-    $statement = $conn->prepare("INSERT INTO hbc353_4.Email (Medicare_Number, Facility_Name, Date, Subject, Body) VALUES(:Medicare_Number, :Facility_Name, :Date, :Subject, :Body)");
+    if(mail($Email, $Subject, $Body, "from:comp353health@gmail.com")){
+        
+    $statement = $conn->prepare("INSERT INTO hbc353_4.Email (Medicare_Number, Facility_Name, Date, Subject, Body, Employee_Email) VALUES(:Medicare_Number, :Facility_Name, :Date, :Subject, :Body, :Employee_Email)");
         $statement->bindParam(':Medicare_Number', $Medicare_Number);
         $statement->bindParam(':Facility_Name', $Facility_Name);
         $statement->bindParam(':Subject', $Subject);
         $statement->bindParam(':Body', $Body);
         $statement->bindParam(':Date', $Date);
+        $statement->bindParam(':Employee_Email', $Email);
         $statement->execute();
         $employee = $statement->fetch(PDO::FETCH_ASSOC);
 
@@ -19,6 +21,7 @@ function sendAndLog($Medicare_Number, $Email, $Facility_Name, $Subject, $Body)
         header("Location: ../emails");
         
     }
+    
 }
 
 ?>
